c7da4ade36340560cccaacd816a8ea0f387ea6dd
Crowdbotics <team@crowdbotics.com>
2021-10-08 14:31:09 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "blue_field_31239",
+  url: "https://blue_field_31239.botics.co",
+  api: "https://blue_field_31239.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
